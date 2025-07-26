23c6e6848346cfe5645767384489eb76c6c7b50e
AtoZproductservices <77913594+AtoZproductservices@users.noreply.github.com>
2025-07-26 06:51:43 +0530

    Create Task 2.py

---
+++ Task 2.py
@@ -0,0 +1,10 @@
+#Task 2: Create a Personalized Greeting
+#1.  Takes a user's first name and last name as input.
+fname = input('Enter your first name: ')
+lname = input('Enter your last name: ')
+
+#2.  Concatenates the first name and last name into a full name.
+fullName = fname + ' ' + lname
+
+#3.  Prints a personalized greeting message using the full name.
+print('Hello, ', fullName , '! Welcome to the Python program.')
